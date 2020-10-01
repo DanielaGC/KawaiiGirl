a48d7ff66169623ffbf95264f168241c1dac6731
DanielaGC <danielagc676@gmail.com>
2020-09-30 23:18:07 -0300

    Update ReadyListener

--- src/events/ReadyListener.ts
+++ src/events/ReadyListener.ts
@@ -7,7 +7,14 @@ export default class ReadyListener extends EventContext {
     }
 
     run() {
+        const status: any = [
+            { name: "Fate/kaleid liner Prisma☆Illya", type: 3 },
+            { name: "Fate/kaleid liner Prisma☆Illya 2wei Herz!", type: 3 },
+            { name: "Fate/kaleid liner Prisma☆Illya 3rei!!", type: 3 },
+            { name: "Fate/kaleid liner Prisma☆Illya: Sekka no Chikai", type: 3 },
+            { name: "Fate/kaleid liner Prisma☆Illya Movie 2", type: 3 }
+        ]
         console.log("Eu estou online!")
-        this.client.editStatus("online", { name: "Fate⭐Kaleid Liner Prisma Illya", type: 3 })
+        setInterval(() => this.client.editStatus("online", status[Math.floor(Math.random() * status.length)]), 15000)
     }
 }
