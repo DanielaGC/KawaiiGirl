8c6dfb4de970374dea53b804f96351e19e5b8f80
DanielaGC <danielagc676@gmail.com>
2021-05-23 23:04:47 -0300

    Update some status

--- src/events/ReadyListener.ts
+++ src/events/ReadyListener.ts
@@ -9,15 +9,14 @@ module.exports = class ReadyListener extends EventListener {
 
   run() {
     const activities: ActivitiesOptions[] = [
-      { name: `Use ${process.env.PREFIX}comandos para saber dos meus comandos` },
-      { name: `Eu conheço ${this.client.users.cache.size} pessoas em ${this.client.guilds.cache.size} servidores diferentes` },
-      { name: `Se precisa de ajuda, dê ${process.env.PREFIX}ajuda` },
-      { name: `Conhecimento para ${this.client.users.cache.size} pessoas!` },
-      { name: 'Doe para mim e ganhe a minha versão premium' },
+      { name: `Use ${process.env.PREFIX}comandos para saber quais são os meus comandos` },
+      { name: `Eu conheço ${this.client.users.cache.size} usuários em ${this.client.guilds.cache.size} servidores diferentes` },
+      { name: `Se precisa de ajuda, use ${process.env.PREFIX}ajuda` },
+      { name: `Conhecimento para ${this.client.users.cache.size} usuários!` },
       { name: `Use ${process.env.PREFIX}convite para poder me adicionar em seu servidor.` },
       { name: `Conectada a ${this.client.channels.cache.size} canais em ${this.client.guilds.cache.size} servidores` },
-      { name: 'Visite o meu WebSite: https://kawaiigirl-website.glitch.me/' },
-      { name: `Posso contar o seu upvote? Se sim use ${process.env.PREFIX}votar e me ajude a compra pão de queijo!` }
+      { name: 'Visite o meu website: https://chinokafuu.glitch.me/' },
+      { name: `Posso contar com o seu voto? Se sim, use ${process.env.PREFIX}votar e me ajude a crescer cada vez mais!` }
     ]
 
     setInterval(() => this.client.user.setPresence({ activities }), 15000)
